07adabc00dd0c98544554cd311957830e0539e85
Matthew Sackman <matthew@wellquite.org>
2017-01-20 18:44:57 +0000

    Slightly extra good. Ref T40

--- example/env/runGosEnv.sh
+++ example/env/runGosEnv.sh
@@ -3,4 +3,4 @@
 DIRECTORY=`dirname $0`
 GOS=/path/to/goshawkdb
 
-$GOS -config $DIRECTORY/config.json --wss -cert $DIRECTORY/clusterKeyPair.pem -dir $DIRECTORY/data
+$GOS -config $DIRECTORY/config.json -wss -cert $DIRECTORY/clusterKeyPair.pem -dir $DIRECTORY/data
