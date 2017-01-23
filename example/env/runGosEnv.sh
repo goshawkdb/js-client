d27f978f6b311777616862bf1f14c07386ef28e0
Adam Iley <adam.iley@db.com>
2017-01-23 21:21:50 +0000

    Updates

--- example/env/runGosEnv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DIRECTORY=`dirname $0`
-GOS=/path/to/goshawkdb
-
-$GOS -config $DIRECTORY/config.json -wss -cert $DIRECTORY/clusterKeyPair.pem -dir $DIRECTORY/data
--- index.js
+++
@@ -1,5 +0,0 @@
-module.exports = require('./src/goshawkdb')
-
-module.exports.utils = require('./src/utils')
-module.exports.Uint64 = require('./src/uint64')
-module.exports.errors = require('./src/errors')
